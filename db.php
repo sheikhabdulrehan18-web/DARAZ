c7969a711fa28051e00762b086ae2fcfebd0057a
sheikhabdulrehan18-web <sheikhabdulrehan18@gmail.com>
2026-03-05 12:49:25 +0500

    Create db.php

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+$host = 'localhost';
+$dbname = 'rsk9_65';
+$username = 'rsk9_65';
+$password = '123456';
+ 
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
+?>
